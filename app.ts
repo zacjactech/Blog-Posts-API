8e13f45c9d5f4d533ee062c912a0b7b900a27062
zacjactech <cb.zacjac@gmail.com>
2024-10-27 03:04:36 +0000

    update port in app,ts

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 
-const port =""
+const port =process.env.PORT
 app.listen(port||3000, ()=>{
   console.log(`Server listening on port ${port||3000}!`)
 })
