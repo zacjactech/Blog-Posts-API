b9b21af1b34ccb25b6a50f04719e19a49aa7f611
Jacob Zaccheous <cb.zacjac@gmail.com>
2024-10-28 14:18:50 +0000

    fix all error

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,17 +5,15 @@ import { MONGO_URI } from './env';
 
 dotenv.config()
 
-const uri =  MONGO_URI
-
-console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const clientOptions : object = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 const connectDB = async () => {
+  await mongoose.connect( MONGO_URI, clientOptions);
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+   
     await mongoose.connection.db?.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
